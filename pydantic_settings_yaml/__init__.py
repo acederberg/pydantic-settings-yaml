b083f7ab464755486f1b8ab292f2b824c8baf6b0
Adrian Cederberg <adrian@mountainvector.com>
2022-10-30 10:00:38 -0600

    It works! Just need to add tests and a README.rst.

---
+++ pydantic_settings_yaml/__init__.py
@@ -0,0 +1,119 @@
+from typing import Callable, Dict, Iterator, List, Optional, Tuple, Union
+
+from pydantic.env_settings import SettingsSourceCallable
+from yaml import safe_load
+
+
+class PydanticSettingsYamlError(Exception):
+    ...
+
+
+def __recursive_merge(
+    to_combine: Optional[Dict],
+    already_combined: Dict,
+) -> None:
+    """Recursive merge helper. Thank you `stack exchange<https://bit.ly/3SJcASN>`!
+
+    :param to_combine: Dictionary to merge in.
+    :param already_combined: Dictionary to merge into.
+    :returns: Nothing.
+    """
+
+    if to_combine is None:
+        return
+    for key, value in to_combine.items():
+        if isinstance(value, dict):
+            __recursive_merge(value, already_combined.setdefault(key, value))
+        else:
+            already_combined[key] = value
+
+
+def _recursive_merge(items: Iterator[Dict]) -> Dict:
+    """Nondestrucively deep merge dictionaries.
+
+    Objects take precendence as presented in the input.
+    :param items: The various items to merge into one object.
+    :returns: Merge produce.
+    """
+
+    out: Dict = {}
+    _ = all(__recursive_merge(item, out) is None for item in items)
+    return out
+
+
+def create_yaml_settings(
+    *filepaths: str,
+    reload: bool = True,
+) -> Callable[[SettingsSourceCallable], Dict]:
+    """Create a ``yaml`` setting loader middleware.
+
+    Using this properly will help you not reload the files provided.
+    To disable this, set :param:``reload`` tu ``True``.
+    :param filepaths: ``YAML`` or ``JSON`` to load.
+    :param reload: Always reload the files when ``True``.
+    :raises ValueError: When :param:``filepaths`` is not sufficiently long,
+        e.g. when it has length 0.
+    :returns: A middleware to load files specified by the filepaths.
+
+
+    Use cases involve nested schemas, which can be helpful when there are a
+    large number of settings that serve various purposes. For instance the
+    flollowing:
+
+    .. literal_include:: ../tests/example.py
+       :language: python
+
+    Would parse the following yaml settings.
+
+    .. literal_include:: ../tests/example.yaml
+       :language: yaml
+
+    """
+
+    n = len(filepaths)
+    if n == 0:
+        raise ValueError("Atleast one file is required.")
+
+    def yaml_loadmanyandvalidate() -> Dict:
+        """Load data and validate that it is sufficiently shaped for
+        ``BaseSettings``.
+
+        :raises PydanticSettingsYamlError: When any of the files do not
+            deserialize to a dictionary.
+        :returns: Loaded files.
+        """
+
+        # bulk load files. use comprehensions.
+        files = {filepath: open(filepath) for filepath in filepaths}
+        loaded_files: Dict[str, Dict] = {
+            file_name: safe_load(file) for file_name, file in files.items()
+        }
+        any(file.close() for file in files.values())
+
+        # Bulk validate. Settings requires key value pairs.
+        isnotadict = tuple(
+            name
+            for name, content in loaded_files.items()
+            if not isinstance(content, dict)
+        )
+        if len(isnotadict) > 0:
+            raise PydanticSettingsYamlError(
+                f"Invalid file format: The files ``{isnotadict}`` do not"
+                f"deserialize to dictionaries."
+            )
+
+        return _recursive_merge(iter(loaded_files.values()))
+
+    if not reload:
+
+        loaded = yaml_loadmanyandvalidate()
+
+        def yaml_settings(settings: SettingsSourceCallable) -> Dict:
+            return loaded
+
+        return yaml_settings
+
+    def yaml_settings(settings: SettingsSourceCallable) -> Dict:
+        return yaml_loadmanyandvalidate()
+
+    return yaml_settings
---
+++ tests/example.py
@@ -0,0 +1,70 @@
+from os import path
+
+from pydantic import BaseModel, BaseSettings
+from pydantic.env_settings import SettingsSourceCallable
+from pydantic_settings_yaml import create_yaml_settings
+
+
+class MySettings(BaseSettings):
+
+    # This is the important stuff that implements the function
+    # in question.
+    class Config:
+
+        # Use reload to determine if create_yaml_settings will
+        # load and parse the provided files every time it is
+        # called.
+        env_yaml_settings = create_yaml_settings(
+            path.realpath(path.join(path.dirname(__file__), "example.yaml")),
+            reload=False,
+        )
+
+        @classmethod
+        def customise_sources(
+            cls,
+            init_settings: SettingsSourceCallable,
+            env_settings: SettingsSourceCallable,
+            file_secret_settings: SettingsSourceCallable,
+        ):
+
+            # The order in which these appear determines their
+            # precendence. So a ``.env`` file could be added to
+            # override the ``YAML`` configuration.
+            return (
+                init_settings,
+                env_settings,
+                file_secret_settings,
+                cls.env_yaml_settings,
+            )
+
+    # Nested configuration example.
+    class MyDataBaseSettings(BaseModel):
+
+        host: str
+        port: int
+        username: str
+        password: str
+
+    myFistSetting: int
+    myDatabaseSettings: MyDataBaseSettings
+
+
+if __name__ == "__main__":
+
+    # Print out parsed settings as q dict/json.
+    import json
+    from shutil import get_terminal_size
+
+    sep = get_terminal_size().columns * "="
+    settings = MySettings()
+
+    print(sep)
+    print("Results parsed from 'example.yaml':")
+    print(
+        json.dumps(
+            settings.dict(),
+            default=str,
+            indent=2,
+        )
+    )
+    print(sep)
