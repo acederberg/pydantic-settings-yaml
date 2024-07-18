deebc43f1b095983dfe2a62b30c414085f0c1e7b
Adrian Cederberg <77076023+acederberg@users.noreply.github.com>
2024-07-18 08:38:06 -0600

    Fix/issue 29 (#30)
    fix(load): Fixed ``yaml_files`` keys cannot be strings
    
    Should fix #29.

---
+++ tests/test_extra.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+from yaml_settings_pydantic import (
+    BaseYamlSettings,
+    YamlFileConfigDict,
+    YamlSettingsConfigDict,
+)
+
+WHATEVER = Path(__file__).parent / "extra" / "whatever.yaml"
+
+
+def test_yaml_files_dict_string_keys() -> None:
+    """For now, make sure that this does not fail."""
+
+    class Settings(BaseYamlSettings):
+        the: str
+        should: str
+        here: str
+
+        model_config = YamlSettingsConfigDict(
+            yaml_files={
+                WHATEVER: YamlFileConfigDict(
+                    subpath="whatever",
+                    required=True,
+                )
+            }
+        )
+
+    settings = Settings.model_validate({})
+    assert settings.the == "heck"
+    assert settings.should == "be"
+    assert settings.here == "dude"
